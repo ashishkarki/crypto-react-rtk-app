8ed8a14b3b98c02d048f9c40940b2aa9294c5f1b
Ashish Karki <ashishk@Ashishs-MacBook-Pro.local>
2022-02-17 16:52:22 +0545

    feat: :construction: adding columns of Statistics to HomePage.js file

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,7 +1,66 @@
 import React from 'react'
+import millify from 'millify'
+import { Col, Row, Statistic, Typography } from 'antd'
+import { REACT_APP_STRING_LITERALS } from '../constants'
+
+const { Title } = Typography
 
 const HomePage = () => {
-  return <div>HomePage</div>
+  return (
+    <>
+      <Title level={2} className="heading">
+        Global Crypto Stats
+      </Title>
+
+      <Row>
+        <Col span={12}>
+          <Statistic
+            title={
+              REACT_APP_STRING_LITERALS.HOMEPAGE_STATS_TITLES
+                .TOTAL_CRYPTOCURRENCIES
+            }
+            value="5"
+          />
+        </Col>
+
+        <Col span={12}>
+          <Statistic
+            title={
+              REACT_APP_STRING_LITERALS.HOMEPAGE_STATS_TITLES.TOTAL_EXCHANGES
+            }
+            value="5"
+          />
+        </Col>
+
+        <Col span={12}>
+          <Statistic
+            title={
+              REACT_APP_STRING_LITERALS.HOMEPAGE_STATS_TITLES.TOTAL_MARKET_CAP
+            }
+            value="5"
+          />
+        </Col>
+
+        <Col span={12}>
+          <Statistic
+            title={
+              REACT_APP_STRING_LITERALS.HOMEPAGE_STATS_TITLES.TOTAL_24H_VOLUME
+            }
+            value="5"
+          />
+        </Col>
+
+        <Col span={12}>
+          <Statistic
+            title={
+              REACT_APP_STRING_LITERALS.HOMEPAGE_STATS_TITLES.TOTAL_MARKETS
+            }
+            value="5"
+          />
+        </Col>
+      </Row>
+    </>
+  )
 }
 
 export default HomePage
--- src/constants.js
+++ src/constants.js
@@ -6,6 +6,13 @@ export const REACT_APP_STRING_LITERALS = {
     EXCHANGES: 'Exchanges',
     NEWS: 'News',
   },
+  HOMEPAGE_STATS_TITLES: {
+    TOTAL_CRYPTOCURRENCIES: 'Total Cryptocurrencies',
+    TOTAL_EXCHANGES: 'Total Exchanges',
+    TOTAL_MARKET_CAP: 'Total Market Cap',
+    TOTAL_24H_VOLUME: 'Total 24h Volume',
+    TOTAL_MARKETS: 'Total Markets',
+  },
 }
 
 export const REACT_APP_ROUTE_NAMES = {
